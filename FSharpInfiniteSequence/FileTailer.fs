1a0d42c90fc14ef255fa01aaf5408d40f387a281
Jimmy Byrd <jimmybyrd@gmail.com>
2015-01-28 23:39:50 -0500

    Added tailer

---
+++ FSharpInfiniteSequence/FileTailer.fs
@@ -0,0 +1,43 @@
+﻿module FileTailer 
+open System
+open System.IO
+open FSharp.Control
+
+let private createWatcher path =
+    let watcher = new FileSystemWatcher()
+    let dirInfo = DirectoryInfo(path)
+    watcher.Path <- dirInfo.Parent.FullName
+    watcher.Filter <- dirInfo.Name
+    watcher.EnableRaisingEvents <- true
+    watcher
+
+let private openFileAsReadOnly path = new StreamReader(new FileStream(path,FileMode.Open, FileAccess.Read, FileShare.ReadWrite ||| FileShare.Delete))
+
+let private tailFileInit path = asyncSeq {
+    let stream = path |> openFileAsReadOnly |> ref
+    let watcher = createWatcher path
+    
+    watcher.Renamed.Subscribe(fun _ ->   
+                                    try
+                                        stream := path |> openFileAsReadOnly
+                                    with
+                                    | ex -> ()) |> ignore //if we couldn't re-open the file, it was probably moved
+                          
+    watcher.Created.Subscribe(fun _ ->   
+                                stream := path |> openFileAsReadOnly) |> ignore
+
+    (!stream).ReadToEndAsync() |> Async.AwaitTask |> ignore
+
+    let rec loop (streamer : ref<StreamReader>) =   asyncSeq{
+        do! Async.Sleep 1
+        yield (!streamer).ReadLine()
+        yield! loop (streamer)
+        }
+    yield! loop (stream)
+}
+
+let private filterOutNullString tailFile =
+    tailFile |> AsyncSeq.filter (String.IsNullOrEmpty >> not)
+
+let TailFile = tailFileInit >> filterOutNullString
+
---
+++ FSharpInfiniteSequence/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open System
+open System.IO
+open FSharp.Control
+
+[<EntryPoint>]
+let main argv = 
+    
+    FileTailer.TailFile @"C:\Users\jimmy_000\Documents\test.txt" 
+        |> AsyncSeq.iter Console.WriteLine
+        |> Async.Start
+ 
+    Console.ReadLine() |> ignore
+    0
