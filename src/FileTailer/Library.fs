2bf607cc931dd3cc37d87a047d34dc7e16b5b01c
Jimmy Byrd <jimmybyrd@gmail.com>
2015-01-29 21:44:15 -0500

    Added files back in

--- src/FileTailer/Library.fs
+++ src/FileTailer/Library.fs
@@ -7,10 +7,47 @@ namespace FileTailer
 ///     let h = Library.hello 1
 ///     printfn "%d" h
 ///
-module Library = 
-  
-  /// Returns 42
-  ///
-  /// ## Parameters
-  ///  - `num` - whatever
-  let hello num = 42
+module FileTailerModule =
+    open System
+    open System.IO
+    open FSharp.Control
+
+    let private openFileAsReadOnly path = 
+        if File.Exists path then
+             new StreamReader(new FileStream(path,FileMode.Open, FileAccess.Read, FileShare.ReadWrite ||| FileShare.Delete)) 
+        else
+             new StreamReader(new MemoryStream()) 
+
+    let private tailFileInit path (getFile : string -> StreamReader) = asyncSeq {
+        let stream = path |> getFile
+        stream.ReadToEnd() |> ignore
+    
+        let rec read (streamer : StreamReader) =   asyncSeq {
+                do! Async.Sleep 1
+                if File.Exists(path) |> not  then 
+                    streamer.Dispose()
+                    yield! tryLoadFile(path)
+                while streamer.EndOfStream |> not do
+                    yield streamer.ReadLine()           
+                yield! read (streamer)
+            }
+            and tryLoadFile(path : string) = asyncSeq {
+                do! Async.Sleep 1
+                if File.Exists(path) |> not  then 
+                    yield! tryLoadFile(path)
+                else 
+                    let newStream = path |> getFile
+                    newStream.BaseStream.Seek(int64 0, SeekOrigin.Begin) |> ignore
+                    yield! read(newStream)
+            } 
+
+        yield! read (stream)
+    }
+
+    let private tailFileInit' path = 
+        tailFileInit path openFileAsReadOnly
+
+    let private filterOutNullString tailFile =
+        tailFile |> AsyncSeq.filter (String.IsNullOrEmpty >> not)
+
+    let TailFile = tailFileInit' >> filterOutNullString
--- tests/FileTailer.Tests/Tests.fs
+++ tests/FileTailer.Tests/Tests.fs
@@ -5,6 +5,5 @@ open NUnit.Framework
 
 [<Test>]
 let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+  let result = FileTailerModule.TailFile "test.txt"
+  Assert.AreEqual(0,0)
