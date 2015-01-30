75e74692de7371078e26a08e14cada36bdb85ca7
Jimmy Byrd <jimmybyrd@gmail.com>
2015-01-29 21:24:08 -0500

    using project scaffold now

---
+++ src/FileTailer/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FileTailer")>]
+[<assembly: AssemblyProductAttribute("FileTailer")>]
+[<assembly: AssemblyDescriptionAttribute("A short tailer in F# that tails a file event after deletes or rotates.  Typically used for logs.")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
