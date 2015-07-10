551adc95e6431627493f51f68fec35b21ac14e93
Jimmy Byrd <jimmybyrd@gmail.com>
2015-07-09 23:55:05 -0400

    Trying out new things

--- src/FileTailer/Library.fs
+++ src/FileTailer/Library.fs
@@ -1,53 +1,76 @@
 namespace FileTailer
 
-/// Documentation for my library
-///
-/// ## Example
-///
-///     let h = Library.hello 1
-///     printfn "%d" h
-///
 module FileTailerModule =
     open System
     open System.IO
     open FSharp.Control
 
-    let private openFileAsReadOnly path = 
-        if File.Exists path then
-             new StreamReader(new FileStream(path,FileMode.Open, FileAccess.Read, FileShare.ReadWrite ||| FileShare.Delete)) 
-        else
-             new StreamReader(new MemoryStream()) 
+//    let private openFileAsReadOnly path = 
+//        if File.Exists path then
+//             new StreamReader(new FileStream(path,FileMode.Open, FileAccess.Read, FileShare.ReadWrite ||| FileShare.Delete)) 
+//        else
+//             new StreamReader(new MemoryStream()) 
+//
+//    let private tailFileInit path (getFile : string -> StreamReader) = asyncSeq {
+//        let stream = path |> getFile
+//        stream.ReadToEnd() |> ignore
+//    
+//        let rec read (streamer : StreamReader) =   asyncSeq {
+//                do! Async.Sleep 1
+//                if File.Exists(path) |> not  then 
+//                    streamer.Dispose()
+//                    yield! tryLoadFile(path)
+//                while not streamer.EndOfStream  do
+//                    yield streamer.ReadLine()           
+//                yield! read (streamer)
+//            }
+//            and tryLoadFile(path) = asyncSeq {
+//                do! Async.Sleep 1
+//                if File.Exists(path) |> not  then 
+//                    yield! tryLoadFile(path)
+//                else 
+//                    let newStream = path |> getFile
+//                    newStream.BaseStream.Seek(int64 0, SeekOrigin.Begin) |> ignore
+//                    yield! read(newStream)
+//            } 
+//
+//        yield! read (stream)
+//    }
+//
+//    let private tailFileInit' path = 
+//        tailFileInit path openFileAsReadOnly
+//
+//    let private filterOutNullString tailFile =
+//        tailFile |> AsyncSeq.filter (String.IsNullOrEmpty >> not)
+//
+//    let TailFile = tailFileInit' >> filterOutNullString
 
-    let private tailFileInit path (getFile : string -> StreamReader) = asyncSeq {
-        let stream = path |> getFile
-        stream.ReadToEnd() |> ignore
-    
-        let rec read (streamer : StreamReader) =   asyncSeq {
-                do! Async.Sleep 1
-                if File.Exists(path) |> not  then 
-                    streamer.Dispose()
-                    yield! tryLoadFile(path)
-                while streamer.EndOfStream |> not do
-                    yield streamer.ReadLine()           
-                yield! read (streamer)
-            }
-            and tryLoadFile(path : string) = asyncSeq {
-                do! Async.Sleep 1
-                if File.Exists(path) |> not  then 
-                    yield! tryLoadFile(path)
-                else 
-                    let newStream = path |> getFile
-                    newStream.BaseStream.Seek(int64 0, SeekOrigin.Begin) |> ignore
-                    yield! read(newStream)
-            } 
-
-        yield! read (stream)
+    let getFile filePath = async{
+            return new StreamReader(new FileStream(filePath,FileMode.Open, FileAccess.Read, FileShare.ReadWrite ||| FileShare.Delete)) 
+        }
+    let getRetryFilePath filePath = async {
+        while not <| File.Exists filePath do
+            do! Async.Sleep 100
+        return! getFile filePath
     }
-
-    let private tailFileInit' path = 
-        tailFileInit path openFileAsReadOnly
-
-    let private filterOutNullString tailFile =
-        tailFile |> AsyncSeq.filter (String.IsNullOrEmpty >> not)
-
-    let TailFile = tailFileInit' >> filterOutNullString
+    
+    let getAsyncFileRead howToGetFile filename = asyncSeq {
+        let! file = howToGetFile filename     
+        
+        let rec readLoop (stream : StreamReader) = asyncSeq {
+            
+           let! line = (stream.ReadLineAsync() |> Async.AwaitTask)
+           if String.IsNullOrEmpty line then 
+                yield! rest (stream)
+           else 
+                yield line
+                yield! readLoop(stream)
+           }
+        and rest (stream : StreamReader) = asyncSeq {
+           do! Async.Sleep 1
+           yield! readLoop stream
+           } 
+        yield! readLoop(file)
+    }
+      
+            
--- src/FileTailer/Script.fsx
+++ src/FileTailer/Script.fsx
@@ -1,8 +1,34 @@
 // Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
+
+#r "../../lib/FSharp.AsyncExtensions.dll"
 #load "Library.fs"
 open FileTailer
+open FSharp.Control
+open System.IO
+open System.Threading
 
-let num = Library.hello 42
-printfn "%i" num
+let source = System.IO.Path.Combine(__SOURCE_DIRECTORY__, "Hello3.txt") 
+
+printfn "%s" source
+
+
+let getFileAsync = FileTailerModule.getAsyncFileRead FileTailerModule.getRetryFilePath 
+
+let readFile = getFileAsync source |> AsyncSeq.iter(fun s -> printfn "%s" s)
+let cToken = new CancellationTokenSource()
+Async.Start(readFile,cToken.Token)
+
+let appendAllLines (path : string) (lines : seq<string>) = File.AppendAllLines(path, lines)
+
+
+async {
+    [1 .. 1000000] 
+    |> Seq.map(string) 
+    |> appendAllLines source
+
+    //|> Seq.iter (fun x -> appendAllLines source [x]) 
+    } |> Async.Start
+
+cToken.Cancel()
